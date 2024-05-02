afb6046ba26d5277a87920dd2ba2490c130758d7
pauleena phan <pauleena2002@gmail.com>
2024-05-02 00:08:20 -0700

    updated to add app provider

--- src/App.js
+++ src/App.js
@@ -6,17 +6,21 @@ import {
 } from "react-router-dom";
 
 import { Login, CreateAccount } from "./pages/account";
+import { UserProvider } from "./pages/userInfo";
 
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />}/>
-        <Route path="/createAcc" element={<CreateAccount/>} />
-      </Routes>
-      
-    </Router>
+    <UserProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />}/>
+          <Route path="/createAcc" element={<CreateAccount/>} />
+        </Routes>
+        
+      </Router>
+    </UserProvider>
+    
   );
 }
 
