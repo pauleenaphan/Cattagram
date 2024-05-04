ded589d499ed3bb73007d1ca57cbd67893b009ea
pauleena phan <pauleena2002@gmail.com>
2024-05-03 22:57:22 -0700

    added cover and settings page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from "react";
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { Login, CreateAccount } from "./pages/account";
+import { Home } from "./pages/homepage"
 import { UserProvider } from "./pages/userInfo";
-
+import { Settings } from "./pages/settings";
+import { Cover } from "./pages/cover";
 
 function App() {
+  // const [isLogged, setLogged] = useState("false");
+
   return (
     <UserProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<Login />}/>
-          <Route path="/createAcc" element={<CreateAccount/>} />
+          <Route path="/" element={<Cover/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/createAcc" element={<CreateAccount/>}/>
+          <Route path="/homepage" element={<Home/>}/>
+          <Route path="/settings" element={<Settings/>}/>
         </Routes>
-        
-      </Router>
+        </Router>
     </UserProvider>
-    
   );
 }
 
