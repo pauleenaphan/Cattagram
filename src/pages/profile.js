c53c35ea6d79cf19b143dd901a21918f5813e3b8
pauleena phan <pauleena2002@gmail.com>
2024-05-06 23:36:30 -0700

    added profile page

---
+++ src/pages/profile.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Navbar } from "./navbar";
+
+export const Profile = () =>{
+    return(
+        <div>
+            <Navbar/>
+            <h1> WE IN PROFILE PAGE</h1>
+        </div>
+    )
+}
