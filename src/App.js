631991aace82a7fa920896fd9fa14f06f0fea824
pauleena phan <pauleena2002@gmail.com>
2024-05-07 22:37:23 -0700

    added global css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import '../src/App';
-
-
+import '../src/style/global.css'
 import { Login, CreateAccount } from "./pages/account";
 import { Home } from "./pages/homepage"
 import { UserProvider } from "./pages/userInfo";
