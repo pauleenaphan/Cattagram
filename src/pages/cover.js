b9e31db3c25dbeff8a6a2b9ff45446c480ab46e1
pauleena phan <pauleena2002@gmail.com>
2024-05-07 22:37:36 -0700

    added loading page

--- src/pages/cover.js
+++ src/pages/cover.js
@@ -1,23 +1,29 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom"; 
 
+import '../style/loading.css';
+import loadingGif from "../imgs/loading.gif"
+
 export const Cover = ()=>{
     const navigate = useNavigate(); 
 
+    //cover page will staay active for 5 seconds
     useEffect(() => {
-        //delay navigation by 3 seconds
         const timeoutId = setTimeout(() => {
             if (localStorage.getItem("isLogged") === "true") {
                 navigate("/homepage");
             } else {
                 navigate("/createAcc");
             }
-        }, 3000); 
+        }, 5000); 
     
         return () => clearTimeout(timeoutId);
     }, [navigate]);
 
     return(
-        <p> Cattagram is loading! </p>
+        <div className="pageContainer">
+            <img className="loadingGif" src={loadingGif} alt="cat loading screen"/>
+        </div>
+        
     )
 }
