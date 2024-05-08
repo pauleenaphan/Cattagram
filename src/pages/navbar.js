dc370cf85101bd5486f7609852f769544dbc6fbd
pauleena phan <pauleena2002@gmail.com>
2024-05-07 22:38:21 -0700

    made navbar positioned fixed

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -5,6 +5,7 @@ import { CgProfile } from "react-icons/cg";
 import { IoHomeOutline, IoSettingsOutline, IoChatbubbleEllipsesOutline  } from "react-icons/io5";
 
 import '../style/nav.css';
+import cattagramLogo from "../imgs/logo/cattagramLogo.png"
 
 //navbar to use on all pages
 export const Navbar = () => {
@@ -12,7 +13,7 @@ export const Navbar = () => {
 
     return (
     <nav className="headerNav">
-        <h1> Cattagram </h1>
+        <img src={cattagramLogo} alt="cattagram logo"/>
         <section className="navigation">
             <IoHomeOutline className="icon" onClick={()=> navigate("/homepage")}/>
             <CgProfile className="icon" onClick={()=> navigate("/profilepage")}/>
