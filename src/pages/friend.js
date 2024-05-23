7d9457c59eb2de70d3382ab9fe0cb7a43dc9cec0
pauleena phan <pauleena2002@gmail.com>
2024-05-23 14:46:24 -0700

    added friend search

--- src/pages/friend.js
+++ src/pages/friend.js
@@ -1,21 +1,117 @@
 import React, { useState } from 'react';
-import { useNavigate } from "react-router-dom";
 import { FiUserPlus } from "react-icons/fi";
 import { Modal } from "flowbite-react";
 import { IoMdNotificationsOutline } from "react-icons/io";
 import { FaSearch } from "react-icons/fa";
+import { collection, getDocs } from "firebase/firestore"; 
+import levenshtein from 'fast-levenshtein';
 
 import '../style/friend.css';
+import { db } from '../firebaseConfig.js';
 import { Navbar } from './navbar';
 
 export const Friend = () =>{
     const [addFriendPopup, setAddFriendPopup] = useState(false); //visbility for addfriend modal
+    const [userPopup, setUserPopup] = useState(false);
+    const [userProfile, setUserProfile] = useState({ 
+        name: "",
+        desc: "",
+        date: "",
+        img: null,
+    })
+    const [userProfilePost, setUserProfilePost] = useState([]); //user post popup
     const [newFriendInput, setNewFriendInput] = useState("");
+    const [friendSearchResult, setFriendSearchResult] = useState([]);
+    const {distance} = require('fastest-levenshtein');
+    const [userFoundStatus, setUserFoundStatus] = useState("");
 
+    //creates a list of users that have the similar name
+    //uses fast-levenshtein to find matching names
     const findUser = async () =>{
+        const results = [];
+        setFriendSearchResult([]); //clear field everytime we search
+        setUserFoundStatus("");
+        try {
+            const usersSnapshot = await getDocs(collection(db, "Users"));
+            //loops thru each of the docs to find a matching username 
+            for (const userDoc of usersSnapshot.docs) {
+                const userInfoSnapshot = await getDocs(collection(db, 'Users', userDoc.id, 'userInfo'));
+                userInfoSnapshot.forEach((userInfoDoc) => {
+                    //smaller value = closest string match
+                    if (distance(userInfoDoc.data().name, newFriendInput) <= (newFriendInput.length/2)){
+                        results.push({
+                            distance: distance(userInfoDoc.data().name, newFriendInput),
+                            id: userInfoDoc.id,
+                            name: userInfoDoc.data().name,
+                            pfp: userInfoDoc.data().pic,
+                            dateJoined: userInfoDoc.data().datejoined,
+                        })
+                    }else{
+                        console.log('not a match')
+                    }
+                });
+            }
+        }catch(error) {
+            console.error('Error finding users:', error);
+        }
 
-        console.log(newFriendInput);
+        if(results.length === 0){
+            setUserFoundStatus("0 Users were found! Try again :(");
+            return
+        }else{
+            if(results.length === 1){
+                setUserFoundStatus(results.length + " User was found!");
+            }else{
+                setUserFoundStatus(results.length + " Users were found!");
+            }
+            
+        }
+        //sorts by lowest distance to highest
+        setFriendSearchResult(results.sort((a, b) => a.distance - b.distance)); 
         setNewFriendInput("");
+    }
+
+    //gets info of the user when pressed on and set its values
+    const fetchUserInfo = async (userName) => {
+        try {
+            const usersSnapshot = await getDocs(collection(db, "Users"));
+            //loops thru each of the docs to find a matching username 
+            for (const userDoc of usersSnapshot.docs) {
+                const userInfoSnapshot = await getDocs(collection(db, 'Users', userDoc.id, 'userInfo'));
+                userInfoSnapshot.forEach((userInfoDoc) => {
+                    const userInfoData = userInfoDoc.data();
+                    console.log("userDoc", userDoc.id);
+                    if (userInfoData.name === userName) {
+                        setUserProfile({
+                            name: userInfoData.name,
+                            desc: userInfoData.profileDesc,
+                            date: userInfoData.datejoined,
+                            img: userInfoData.pic,
+                        })
+                        //set profile post BEFORE we call the popup to open 
+                        //also need to pass in the userDoc id rather than setting it to a prop so it updates in time
+                        getUserProfilePost(userDoc.id);
+                        setUserPopup(true);    
+                    }
+                });
+            }
+        }catch(error) {
+            console.error('Error fetching user info:', error);
+        }
+    };
+
+    //gets all the post that the user has posted
+    const getUserProfilePost = async (userEmail) =>{
+        const post = await getDocs(collection(db, "Users", userEmail, "post"));
+        const postReceived = post.docs.map(doc =>({
+            id: doc.id,
+            title: doc.data().title,
+            desc: doc.data().desc,
+            img: doc.data().img,
+            user: doc.data().user,
+            date: doc.data().date,
+        }))
+        setUserProfilePost(postReceived);
     }
 
     return(
@@ -27,19 +123,69 @@ export const Friend = () =>{
                     <Modal.Header className="modalHeader"></Modal.Header>
                     <div className="friendBodyContainer">
                         <div className="friendHeadContainer">
-                            <h1> Add a new friend </h1>
+                            <h1> Find a cool cat </h1>
                             <div className="searchContainer">
                                 <input type="text" value = {newFriendInput} placeholder="Enter username" className="searchInput" onChange={(e) => {setNewFriendInput(e.target.value)}}></input>
-                                <FaSearch onClick={findUser}/>
+                                <FaSearch class="icon" onClick={findUser}/>
                             </div>
-                            
+                            <p id="userFoundStatus"> {userFoundStatus} </p>
                         </div>
                         <Modal.Body>
-                            <p> list of names that somewhat match </p>
+                            <div className="matchedUserOuterContainer">
+                                {friendSearchResult.map(user =>(
+                                    <div key={user.id} className="matchUserContainer">
+                                        <img src={user.pfp} onClick={() => {fetchUserInfo(user.name)}} alt="userpfp"/>
+                                        <div className="userDescOuterContainer">
+                                            <h1 onClick={() => {fetchUserInfo(user.name)}}> {user.name} </h1>
+                                            <div className="userDescContainer">
+                                                <p> {user.dateJoined} </p> 
+                                                <button className="addFriendBtn"> + Add Friend </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
                         </Modal.Body>
                     </div>  
                 </Modal>
             </>
+            )}
+            {userPopup &&(
+                <>
+                    <div className="overlay" onClick={() => setUserPopup(false)}></div>
+                    <Modal show={userPopup} onClose={() => setUserPopup(false)} className="userProfileModalInFriend">
+                        <Modal.Header className="modalHeader"></Modal.Header>
+                        <div className="userBodyModalContainer">
+                            <Modal.Body>
+                            <section className="profileContainer">
+                                <img src={userProfile.img} alt="userPfp"/>
+                                    <div className="profileFriendDescContainer">
+                                        <h1> {userProfile.name} </h1>
+                                        <p> {userProfile.desc} </p>
+                                        <p> {userProfile.date} </p>
+                                    </div>
+                            </section>
+                            <section className="userProfilePostContainer">
+                                {userProfilePost.map(post => (
+                                    <div key={post.id} className="userPostContainer">
+                                        <div className="nameDateContainer">
+                                            <h1 className="userPostName">{post.user}</h1>
+                                            <p className="postDate">{post.date}</p>
+                                        </div>
+                                        {post.img && (
+                                        <img src={post.img} alt="user post" />
+                                        )}
+                                        <div className="postBodyContainer">
+                                            <h2>{post.title}</h2>
+                                            <p className="postDesc">{post.desc}</p>
+                                        </div>
+                                    </div>
+                                ))}
+                            </section>
+                            </Modal.Body>
+                        </div>
+                    </Modal>
+                </>
             )}
             <div className="tempBtnContainer" onClick={() =>{setAddFriendPopup(!addFriendPopup)}}>
                 <FiUserPlus className="postIcon"/>
